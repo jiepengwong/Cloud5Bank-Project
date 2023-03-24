4a50310e76808ea71e857b585f5d135afbbe0af0
Song Yu Xiang <yxsong.2020@scis.smu.edu.sg>
2023-03-24 15:13:10 +0800

    Fixing with Cognito

--- frontend/src/Pages/Home/Homepage.jsx
+++ frontend/src/Pages/Home/Homepage.jsx
@@ -4,37 +4,73 @@ import CustomizedDialogs from '../../components/dialog/Dialog'
 
 import Register from '../../components/register/Register'
 import Login from '../../components/login/Login'
+import { useNavigate } from 'react-router-dom'
+
+// Import AWS
+import AWS from 'aws-sdk';
+
+AWS.config.update({ region: 'ap-southeast-1' });
 
 function Homepage() {
+  const navigate = useNavigate();
+  
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const code = urlSearchParams.get('code');
 
-  return (
-    <div className='homepage'>
-      <img src='https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3JtMjA4YmF0Y2g0LWt3YW4tMDEtZy5qcGc.jpg' alt='bg'  className='homepage-bg'></img>
-      <header>
-        <img src='https://api.iconify.design/bxs/bank.svg?color=white' alt='LOGO'></img>
-        <h2>Sprints Bank</h2>
-      </header>
+  if (!code) {
+    return (
+      <div className='homepage'>
+        <img src='https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3JtMjA4YmF0Y2g0LWt3YW4tMDEtZy5qcGc.jpg' alt='bg'  className='homepage-bg'></img>
+        <header>
+          <img src='https://api.iconify.design/bxs/bank.svg?color=white' alt='LOGO'></img>
+          <h2>Sprints Bank</h2>
+        </header>
+  
+        <main>
+          <section>
+  
+            <h1>The bank you trust</h1>
+            <p>Spend, save, invest, and control your financial life</p>
+  
+            <div className='homepage-sign-btns'>
+              <CustomizedDialogs title='Sign in' btn='Sign in'>
+                <Register/>
+              </CustomizedDialogs>
+  
+  
+            </div>
+  
+          </section>
+        </main>
+      </div>
+    )
+  }
+  else {
+    // Use the code to check with the Cognito Side 
+    const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
-      <main>
-        <section>
+    async function getUserInfo(code) {
+      try {
+        const response = await cognitoidentityserviceprovider.getUser({
+          AccessToken: code
+        }).promise();
 
-          <h1>The bank you trust</h1>
-          <p>Spend, save, invest, and control your financial life</p>
+        return response.UserAttributes.reduce((acc, curr) => {
+          console.log(curr.Value)
+          acc[curr] = curr.Value;
+          return acc;
+        }, {});
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    }
+    const userInfo = getUserInfo(code);
+    console.log(userInfo)
 
-          <div className='homepage-sign-btns'>
-            <CustomizedDialogs title='Sign in' btn='Sign in'>
-              <Login/>
-            </CustomizedDialogs>
-
-            <CustomizedDialogs title='Sign up' btn='Sign up' style={{color: "#007bff"}}>
-              <Register />
-            </CustomizedDialogs>
-          </div>
-
-        </section>
-      </main>
-    </div>
-  )
+    // Populate the Use Context. 
+    navigate("/adminpanel/*");
+  }
 }
 
 export default Homepage
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,22 +8,76 @@ import {QueryClientProvider, QueryClient} from 'react-query'
 import { AuthProvider } from './context/Auth-context';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
+// import awsconfig from './aws-exports';
 
-const queryClient = new QueryClient()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <AuthProvider>
-        <QueryClientProvider client={queryClient}>
+// Amplify AWS  
+import Amplify from './aws-exports';
+// Configure AWS SDK
+import AWS from 'aws-sdk';
+import awsconfig from './aws-exports';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
+
+// Configure AWS Amplify
+// Amplify.configure(awsconfig);
+
+const userPool = new CognitoUserPool({
+  UserPoolId: 'ap-southeast-1_uldoanBJO',
+  ClientId: 's3qtr2qr1m73t6keq2b9t3sk4',
+});
+
+const cognitoUser = userPool.getCurrentUser();
+
+if (cognitoUser != null) {
+  cognitoUser.getSession((err, session) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'your_identity_pool_id',
+      Logins: {
+        [`cognito-idp.${AWS.config.region}.amazonaws.com/${'your_user_pool_id'}`]: session.getIdToken().getJwtToken(),
+      },
+    });
+
+    const queryClient = new QueryClient();
+
+    ReactDOM.render(
+      <BrowserRouter>
+        <React.StrictMode>
+          <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+              <ThemeProvider theme={theme}>
+                <App />
+              </ThemeProvider>
+            </QueryClientProvider>
+          </AuthProvider>
+        </React.StrictMode>
+      </BrowserRouter>,
+      document.getElementById('root')
+    );
+  });
+} 
+else {
+  const queryClient = new QueryClient();
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <AuthProvider>
+          <QueryClientProvider client={queryClient}>
             <ThemeProvider theme={theme}>
               <App />
             </ThemeProvider>
-        </QueryClientProvider>
-      </AuthProvider>
-    </React.StrictMode>
-  </BrowserRouter>
-);
+          </QueryClientProvider>
+        </AuthProvider>
+      </React.StrictMode>
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
 
 reportWebVitals();
